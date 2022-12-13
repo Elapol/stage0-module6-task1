a0aa407cb06a98d6eac9b464a8a454327fe6312c
elyad <ella.dikovich@gmail.com>
2022-12-13 19:27:43 +0100

    elkaaaaaaa

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,43 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
-}
+    private static String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("black", 4, true);
+        Animal.getDescription("black", 1, true);
+    }
+
+    static void getDescription(String color, int numberOfPaws, boolean hasFur) {
+        if (numberOfPaws > 1 && hasFur == true) {
+            String resultColor = "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and a fur.";
+            System.out.println(resultColor);
+        } else if (numberOfPaws == 1 && hasFur == true) {
+            String resultColor = "This animal is mostly " + color + ". It has " + numberOfPaws + " paw and a fur.";
+            System.out.println(resultColor);
+        } else if (numberOfPaws > 1 && hasFur == false) {
+            String resultColor = "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and no fur.";
+            System.out.println(resultColor);
+        } else if (numberOfPaws == 1 && hasFur == false) {
+            String resultColor = "This animal is mostly " + color + ". It has " + numberOfPaws + " paw and no fur.";
+            System.out.println(resultColor);
+        }
+    }
+
+
+    }
+
+
+
+
+
+
+
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,18 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+    public Bird(){
+        super("blue",2,false);
+    }
+    public static void main(String[] args) {
+        Bird.getDescription("blue", 2,false);
 
-}
+    }
+    static void getDescription(String color, int numberOfPaws, boolean hasFur) {
+        String resultColor = "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and no fur. Moreover, it has 2 wings and can fly.";
+        System.out.println(resultColor);
+    }
+
+    }
+
+
